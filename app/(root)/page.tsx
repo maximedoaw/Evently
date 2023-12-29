82dd6f8ea1912a8d5af643cf6d14f0adc79b3b7e
maximedoaw <maximedoaw204@gmail.com>
2023-12-29 12:09:00 -0800

    Mongodb Connection

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
@@ -6,10 +9,34 @@ export default function Home() {
       <div className="wrapper grid grid-cols-1 gap-3 md:grid-cols-2
       2xl:gap-0">
         <div className="flex flex-col justify-center gap-8">
-          <div className="h1-bold">
+          <h1 className="h1-bold">
           Host, Connect, Celebrate: Your Events, Our Platform!
-          </div>
+          </h1>
+          <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+          <Button size={'lg'}  className="button w-full
+          sm:w-fit" >
+          <Link href="#event">
+          Explore now
+          </Link>
+          </Button>
         </div>
+
+        <Image
+        src="/assets/images/hero.png"
+        alt="hero"
+        width={500}
+        height={500}
+        className="max-h-[70vh] object-center object-contain
+         2xl:max-h-[50vh]"
+        />
+      </div>
+    </section>
+    <section id="events" className="wrapper my-8 flex flex-col gap-8 
+    md:gap-12">
+      <h2 className="h2-bold">Trust by <br /> Thousands Events</h2>
+      <div className="flex flex-col w-full gap-5 md:flex-row">
+        Search
+        CategoryFilter
       </div>
     </section>
     </>
